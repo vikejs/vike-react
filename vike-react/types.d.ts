93b83833338db424ad0f0d82b63364ee4e6298c6
Romuald Brillout <git@brillout.com>
2023-09-08 08:55:41 +0200

    fix: remove now useless type re-exports

--- vike-react/types.d.ts
+++
@@ -1,6 +0,0 @@
-export type {
-  PageContextBuiltIn,
-  PageContextBuiltInServer,
-  PageContextBuiltInClientWithClientRouting as PageContextBuiltInClient
-} from 'vite-plugin-ssr/types'
-import type { Component } from './renderer/types'

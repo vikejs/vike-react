9414f6b339cf52b3cbdfa462d5ecc5f5ae315a08
Yusaku <hitoyasablue@gmail.com>
2023-11-23 18:35:32 +0900

    rename props

--- vike-react/renderer/ClientOnly.tsx
+++ vike-react/renderer/ClientOnly.tsx
@@ -3,20 +3,20 @@ export { ClientOnly }
 import React, { ComponentType, ReactNode, Suspense, lazy, useEffect, useState } from 'react'
 
 type ClientOnlyProps<T> = {
-  component: () => Promise<{ default: React.ComponentType<T> }>
-  componentRenderer: (Component: React.ComponentType<T>) => ReactNode
+  load: () => Promise<{ default: React.ComponentType<T> }>
+  children: (Component: React.ComponentType<T>) => ReactNode
   fallback: ReactNode
 }
 
-function ClientOnly<T>({ component, componentRenderer, fallback }: ClientOnlyProps<T>) {
+function ClientOnly<T>({ load, children, fallback }: ClientOnlyProps<T>) {
   const [Component, setComponent] = useState<ComponentType<any> | null>(null)
 
   useEffect(() => {
     const loadComponent = () => {
       const Component = lazy(() =>
-        component()
+        load()
           .then((LoadedComponent) => {
-            return { default: () => componentRenderer(LoadedComponent.default) }
+            return { default: () => children(LoadedComponent.default) }
           })
           .catch((error) => {
             console.error('Component loading failed:', error)
@@ -27,7 +27,7 @@ function ClientOnly<T>({ component, componentRenderer, fallback }: ClientOnlyPro
     }
 
     loadComponent()
-  }, [component, componentRenderer])
+  }, [load, children])
 
   return <Suspense fallback={fallback}>{Component ? <Component /> : null}</Suspense>
 }

e86719fc358fe7c6d1e743c7c17d12c870fefcc6
Dániel Horváth <horvath.daniel@lexunit.hu>
2023-12-27 14:58:23 +0100

    minor

--- packages/vike-react-zustand/renderer/context.ts
+++ packages/vike-react-zustand/renderer/context.ts
@@ -2,7 +2,7 @@ import React, { createContext } from 'react'
 import { CreateStore, StoreApi } from '../src/types.js'
 import { getGlobalObject } from './utils/getGlobalObject.js'
 
-const globalObject = getGlobalObject('ZustandServerSide.ts', {
+const globalObject = getGlobalObject('VikeReactZustandContext.ts', {
   createStore: undefined as CreateStore | undefined,
   context: createContext<StoreApi | undefined>(undefined)
 })

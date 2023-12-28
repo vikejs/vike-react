8620dac56ef250adc0386c0f1b05df438ef34b61
Dániel Horváth <horvath.daniel@lexunit.hu>
2023-12-28 17:19:48 +0100

    remove config

--- packages/vike-react-zustand/renderer/+config.h.ts
+++ packages/vike-react-zustand/renderer/+config.h.ts
@@ -1,9 +1,4 @@
 export default {
   VikeReactZustandWrapper: 'import:vike-react-zustand/renderer/VikeReactZustandWrapper:default',
-  passToClient: ['vikeReactZustand'],
-  meta: {
-    vikeReactZustand: {
-      env: { server: true, client: true }
-    }
-  }
+  passToClient: ['vikeReactZustand']
 }

4f493b0773212137bbb6e4d5c6c3eacdbf803115
Dániel Horváth <horvath.daniel@lexunit.hu>
2023-12-29 00:11:18 +0100

    add types

--- packages/vike-react-zustand/src/index.tsx
+++ packages/vike-react-zustand/src/index.tsx
@@ -3,9 +3,42 @@ export { create, server }
 import { useContext } from 'react'
 import { getContext, setCreateStore } from './renderer/context.js'
 import { create as create_ } from 'zustand'
+import type { StoreMutatorIdentifier, UseBoundStore, Mutate, StoreApi as ZustandStoreApi } from 'zustand'
 
-function create(createStore: any): any {
-  setCreateStore(() => {
+type Create = {
+  <T, Mos extends [StoreMutatorIdentifier, unknown][] = []>(
+    initializer: StateCreator<T, [], Mos>
+  ): UseBoundStore<Mutate<ZustandStoreApi<T>, Mos>>
+  <T>(): <Mos extends [StoreMutatorIdentifier, unknown][] = []>(
+    initializer: StateCreator<T, [], Mos>
+  ) => UseBoundStore<Mutate<ZustandStoreApi<T>, Mos>>
+  /**
+   * @deprecated Use `useStore` hook to bind store
+   */
+  <S extends ZustandStoreApi<unknown>>(store: S): UseBoundStore<S>
+}
+
+type Get<T, K, F> = K extends keyof T ? T[K] : F
+export type StateCreator<
+  T,
+  Mis extends [StoreMutatorIdentifier, unknown][] = [],
+  Mos extends [StoreMutatorIdentifier, unknown][] = [],
+  U = T
+> = ((
+  setState: Get<Mutate<ZustandStoreApi<T>, Mis>, 'setState', never>,
+  getState: Get<Mutate<ZustandStoreApi<T>, Mis>, 'getState', never>,
+  store: Mutate<ZustandStoreApi<T>, Mis>
+) => U) & {
+  $$storeMutators?: Mos
+}
+
+const create: Create = ((createState: any) => {
+  return createState ? createImpl(createState) : createImpl
+}) as any
+
+function createImpl(createStore: any): any {
+  // @ts-ignore
+  setCreateStore((pageContext: any) => {
     return create_(createStore)
   })
 
@@ -25,4 +58,13 @@ function server<T extends Record<string, any>>(fn: () => T) {
     return fn()
   }
   return {} as T
+}
+type StoreAndHook = ReturnType<typeof create>
+function withPageContext<S extends StoreAndHook>(storeCreatorCreatorFn: (pageContext: StoreAndHook) => S) {
+  //@ts-ignore
+  // createImpl._withPageContext_ = storeCreatorFn
+  // const storeCreatorFn = () => {
+  //   storeCreatorCreatorFn(pageContext)
+  //   return createImpl(storeCreatorFn)
+  // }
 }

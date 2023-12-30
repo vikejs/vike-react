0a6a80d7673080af938be8234db1f6d4b4d79e0d
Romuald Brillout <git@brillout.com>
2023-12-30 12:04:53 +0100

    minor

--- packages/vike-react-zustand/src/utils/assert.ts
+++ packages/vike-react-zustand/src/utils/assert.ts
@@ -1,4 +1,6 @@
 export function assert(condition: unknown): asserts condition {
   if (condition) return
-  throw new Error('You stumbled upon a bug in the source code of vite-react-zustand, reach out to a maintainer.')
+  throw new Error(
+    "You stumbled upon a bug in vike-react-zustand's source code. Reach out on GitHub and we will fix the bug."
+  )
 }

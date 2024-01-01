ad1866d0462910c99b285f372f62fa064aace573
Romuald Brillout <git@brillout.com>
2024-01-01 21:55:55 +0100

    comment

--- packages/vike-react-zustand/src/renderer/types.d.ts
+++ packages/vike-react-zustand/src/renderer/types.d.ts
@@ -1,5 +1,7 @@
 export type {}
 
+// The types we add here aren't visible to the user (because this file only matches the TypeScript rootDir of packages/vike-react-zustand/tsconfig.json)
+
 declare global {
   namespace Vike {
     interface PageContext {

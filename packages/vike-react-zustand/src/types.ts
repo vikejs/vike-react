6aca5bbbf8ed659ae20f30040a0fd1a59260802a
Dániel Horváth <horvath.daniel@lexunit.hu>
2024-01-02 21:47:24 +0100

    fix types

--- packages/vike-react-zustand/src/types.ts
+++ packages/vike-react-zustand/src/types.ts
@@ -1,31 +1,20 @@
-import type { Mutate, StateCreator, StoreMutatorIdentifier, create, StoreApi } from 'zustand'
+export {}
 
-type ExtractState<S> = S extends {
-  getState: () => infer T
-}
-  ? T
-  : never
+import type { StateCreator, StoreApi, StoreMutatorIdentifier, create } from 'zustand'
+
 export type StoreApiAndHook = ReturnType<typeof create>
-export type StoreApiOnly<Store extends StoreApiAndHook> = Pick<Store, keyof StoreApi<unknown>>
-
-// Copied from zustand, but removed the store api
-export type StoreHookOnly<Store> = {
-  (): ExtractState<Store>
-  <U>(selector: (state: ExtractState<Store>) => U): U
+export type StoreApiOnly<T> = StoreApi<T>
+export type StoreHookOnly<T> = {
+  (): T
+  <U>(selector: (state: T) => U): U
   /**
    * @deprecated Use `createWithEqualityFn` from 'zustand/traditional'
    */
-  <U>(selector: (state: ExtractState<Store>) => U, equalityFn: (a: U, b: U) => boolean): U
-} // & S <-- removed the store api, because it's misleading in an SSR app
+  <U>(selector: (state: T) => U, equalityFn: (a: U, b: U) => boolean): U
+}
 export type Create = {
-  <T, Mos extends [StoreMutatorIdentifier, unknown][] = []>(
-    initializer: StateCreator<T, [], Mos>
-  ): StoreHookOnly<Mutate<StoreApi<T>, Mos>>
+  <T, Mos extends [StoreMutatorIdentifier, unknown][] = []>(initializer: StateCreator<T, [], Mos>): StoreHookOnly<T>
   <T>(): <Mos extends [StoreMutatorIdentifier, unknown][] = []>(
     initializer: StateCreator<T, [], Mos>
-  ) => StoreHookOnly<Mutate<StoreApi<T>, Mos>>
-  /**
-   * @deprecated Use `useStore` hook to bind store
-   */
-  <Store extends StoreApi<unknown>>(store: Store): StoreHookOnly<Store>
+  ) => StoreHookOnly<T>
 }

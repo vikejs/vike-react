f4967b36fc4a30299c0c261d8e5cedbfb166d3fc
Dániel Horváth <horvath.daniel@lexunit.hu>
2024-01-07 13:20:18 +0100

    add comments and improve regex

--- examples/zustand/vite.config.ts
+++ examples/zustand/vite.config.ts
@@ -2,12 +2,20 @@ import react from '@vitejs/plugin-react'
 import vike from 'vike/plugin'
 import type { Plugin, UserConfig } from 'vite'
 
-const ids = new Set()
 export default {
   plugins: [react(), vike(), vikeReactZustandPlugin()]
 } satisfies UserConfig
 
+const hotReloaderCode = `
+if (import.meta.hot) {
+  import.meta.hot.accept(() => {
+    window.location.reload()
+  })
+}
+`
+
 function vikeReactZustandPlugin(): Plugin {
+  const ids = new Set()
   return {
     name: 'vikeReactZustand',
     enforce: 'pre',
@@ -16,27 +24,23 @@ function vikeReactZustandPlugin(): Plugin {
       if (id.includes('node_modules')) {
         return
       }
-      // Playground: https://regex101.com/r/1RThko/1
+      // Playground: https://regex101.com/r/qFAOq5/1
       const hasCreate =
-        /import(?:[\s\w,]*\{[\s\w,]*)create(?:[\s\w,]*\}[\s\w,]*)from\s*["']vike-react-zustand["']/.test(code)
+        /import(?:[\s\w,]*\{[\s\w,]*)(?<!as[\w\s]*)create[\s,}]+(?!as)[\s\w}]*from\s*["']vike-react-zustand["']/.test(
+          code
+        )
       if (!hasCreate) {
         return
       }
 
-      // Playground: https://regex101.com/r/Yqd80A/1
-      code = code.replace(/create\s*(?:<[\s\w<>:{}[\]]*)?(?:\([\w'"`{}()\s]*?\))?\s*?\(/g, (match, position) => {
+      // Playground: https://regex101.com/r/K8paF7/1
+      code = code.replace(/create\s*(?:<[\s\w<>:.&|{}[\],="]*)?(?:\([\w'"`{}()\s]*?\))?\s*?\(/g, (match, position) => {
         const key = simpleHash(`${id}:${position}`)
         return `${match}'${key}',`
       })
 
       ids.add(id)
-      code =
-        code +
-        `if (import.meta.hot) {
-          import.meta.hot.accept(() => {
-            window.location.reload()
-          })
-        }`
+      code = code + hotReloaderCode
 
       let end_time = process.hrtime(start_time)
       // Print the Execution time.

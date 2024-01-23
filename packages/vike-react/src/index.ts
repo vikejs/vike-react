0373541fd170f09e152854796c64a747f4652c05
Romuald Brillout <git@brillout.com>
2024-01-23 17:52:42 +0100

    typo

--- packages/vike-react/src/index.ts
+++ packages/vike-react/src/index.ts
@@ -1,3 +1,3 @@
 // TODO/next-major-release: remove this file/export
-console.warn("[vike-react][warning][deprecation] Replace `import vikeReact from 'vike-react'` with `import vikeReact from 'vike-react/config'")
+console.warn("[vike-react][warning][deprecation] Replace `import vikeReact from 'vike-react'` with `import vikeReact from 'vike-react/config'`")
 export { config } from './+config.js'

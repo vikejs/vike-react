4ab380a9a365c149f31e6edd3fd336fd28ba0dde
Romuald Brillout <git@brillout.com>
2024-01-23 17:57:53 +0100

    fix: improve deprecation warning

--- packages/vike-react/src/index.ts
+++ packages/vike-react/src/index.ts
@@ -1,3 +1,5 @@
 // TODO/next-major-release: remove this file/export
-console.warn("[vike-react][warning][deprecation] Replace `import vikeReact from 'vike-react'` with `import vikeReact from 'vike-react/config'`")
+console.warn(
+  "[vike-react][warning][deprecation] Replace `import vikeReact from 'vike-react'` with `import vikeReact from 'vike-react/config'` (typically in your /pages/+config.h.js)"
+)
 export { config } from './+config.js'

eca08f1fc87ffad708f0d0cbd545e8bfeedbc957
Romuald Brillout <git@brillout.com>
2024-01-23 17:38:58 +0100

    minor

--- packages/vike-react/src/index.ts
+++ packages/vike-react/src/index.ts
@@ -1,3 +1,3 @@
-// TODO/next-major-release: remove this
-export { config } from './+config.js'
+// TODO/next-major-release: remove this file/export
 console.warn("[vike-react][warning][deprecation] Replace `import vikeReact from 'vike-react'` with `import vikeReact from 'vike-react/config'")
+export { config } from './+config.js'

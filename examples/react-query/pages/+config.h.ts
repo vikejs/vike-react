8ca59e59591a5fbddbd8b0005ab24faf9b109809
Romuald Brillout <git@brillout.com>
2024-01-23 17:38:42 +0100

    update examples

--- examples/react-query/pages/+config.h.ts
+++ examples/react-query/pages/+config.h.ts
@@ -2,7 +2,7 @@ import type { Config } from 'vike/types'
 import Layout from '../layouts/LayoutDefault'
 import Head from '../layouts/HeadDefault'
 import logoUrl from '../assets/logo.svg'
-import vikeReact from 'vike-react'
+import vikeReact from 'vike-react/config'
 import vikeReactQuery from 'vike-react-query/config'
 
 // Default configs (can be overridden by pages)

639f86d21e4016fa225887cb06702416c24d5742
Romuald Brillout <git@brillout.com>
2024-02-14 16:11:27 +0100

    set Vike extension name

--- packages/vike-react-zustand/src/renderer/+config.ts
+++ packages/vike-react-zustand/src/renderer/+config.ts
@@ -1,4 +1,6 @@
 export default {
+  // @ts-ignore Remove this ts-ignore once Vike's new version is released.
+  name: 'vike-react-zustand',
   VikeReactZustandWrapper: 'import:vike-react-zustand/renderer/VikeReactZustandWrapper:default',
   passToClient: ['_vikeReactZustand']
 }

f0632a3d356c6e818ea7cd85bb2caba4cd8fe9e8
Romuald Brillout <git@brillout.com>
2024-03-31 19:08:53 +0200

    chore

--- packages/vike-react-query/vitest.config.ts
+++ packages/vike-react-query/vitest.config.ts
@@ -2,6 +2,8 @@ import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
   test: {
+    // test/**/*.test.ts => @brillout/test-e2e
+    include: ['**/*.spec.*'],
     environment: 'jsdom'
   }
 })

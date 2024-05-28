2cc16cfa7a5b434031c528604d3c10c758678a31
Romuald Brillout <git@brillout.com>
2024-05-28 12:30:08 +0200

    fix: remove counterproductive assert() (fix #115)

--- packages/vike-react/src/hooks/usePageContext.tsx
+++ packages/vike-react/src/hooks/usePageContext.tsx
@@ -4,14 +4,12 @@ export { PageContextProvider }
 import React, { useContext } from 'react'
 import { getGlobalObject } from '../utils/getGlobalObject.js'
 import type { PageContext } from 'vike/types'
-import { assert } from '../utils/assert.js'
 
 const globalObject = getGlobalObject('PageContextProvider.ts', {
   reactContext: React.createContext<PageContext>(undefined as never)
 })
 
 function PageContextProvider({ pageContext, children }: { pageContext: PageContext; children: React.ReactNode }) {
-  assert(pageContext)
   const { reactContext } = globalObject
   return <reactContext.Provider value={pageContext}>{children}</reactContext.Provider>
 }
@@ -24,9 +22,5 @@ function PageContextProvider({ pageContext, children }: { pageContext: PageConte
 function usePageContext(): PageContext {
   const { reactContext } = globalObject
   const pageContext = useContext(reactContext)
-  /* React throws an error upon wrong hook usage, so I guess a nice error message isn't needed? And I guess we can therefore assume and assert pageContext to have been provided? Let's see if users report back an assert() failure.
-  if (!pageContext) throw new Error('<PageContextProvider> is needed for being able to use usePageContext()')
-  */
-  assert(pageContext)
   return pageContext
 }
--- packages/vike-react/src/utils/assert.ts
+++
@@ -1,6 +0,0 @@
-export function assert(condition: unknown): asserts condition {
-  if (condition) return
-  throw new Error(
-    'You stumbled upon a vike-react bug. Go to https://github.com/vikejs/vike-react/issues/new and copy-paste this error. A maintainer will fix the bug (usually under 24 hours).'
-  )
-}

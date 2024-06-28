d88ebcc19ad21b7529ec44cccecfa61a4bdba164
Dániel Horváth <horvath.daniel@lexunit.hu>
2024-06-28 03:24:41 +0200

    fix no stream error

--- packages/vike-react/shared/hooks/useNavigate.tsx
+++ packages/vike-react/shared/hooks/useNavigate.tsx
@@ -5,10 +5,16 @@ import { redirect } from 'vike/abort'
 type NavigateOptionsServer = { statusCode?: 301 | 302 | undefined }
 
 function useNavigate() {
-  const stream = useStream()
+  let stream: ReturnType<typeof useStream> | null = null
+  try {
+    stream = useStream()
+  } catch (error) {
+    // Don't throw if streaming is disabled
+    // The redirect will be sent in the location header
+  }
   const navigateFn = (url: string, options?: NavigateOptionsServer) => {
     if (stream) {
-      //TODO: If the headers are already sent, make sure this is flushed before aborting on the server
+      // TODO: If the headers are already sent, make sure this is flushed before aborting on the server
       // Otherwise the client will render the tree again, and navigate client-side at a later point
       stream.injectToStream(`<script>window.location.href="${url}"</script>`)
     }

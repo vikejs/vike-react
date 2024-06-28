28abbba9debd1334f04128a37be35015961a0505
Dániel Horváth <horvath.daniel@lexunit.hu>
2024-06-28 03:13:10 +0200

    update types

--- packages/vike-react/client/hooks/useNavigate.tsx
+++ packages/vike-react/client/hooks/useNavigate.tsx
@@ -1,6 +1,11 @@
 export { useNavigate }
 import { navigate } from 'vike/client/router'
 
+type NavigateOptionsClient = Parameters<typeof navigate>[1]
+type NavigateOptionsServer = { statusCode?: 301 | 302 | undefined }
+type NavigateOptions = NavigateOptionsClient & NavigateOptionsServer
+type NavigateFn = (url: string, options?: NavigateOptions) => void
+
 function useNavigate() {
-  return navigate
+  return navigate as NavigateFn
 }
--- packages/vike-react/shared/hooks/useNavigate.tsx
+++ packages/vike-react/shared/hooks/useNavigate.tsx
@@ -2,16 +2,18 @@ export { useNavigate }
 import { useStream } from 'react-streaming'
 import { redirect } from 'vike/abort'
 
+type NavigateOptionsServer = { statusCode?: 301 | 302 | undefined }
+
 function useNavigate() {
   const stream = useStream()
-  const navigateFn = (to: string) => {
+  const navigateFn = (url: string, options?: NavigateOptionsServer) => {
     if (stream) {
       //TODO: If the headers are already sent, make sure this is flushed before aborting on the server
       // Otherwise the client will render the tree again, and navigate client-side at a later point
-      stream.injectToStream(`<script>window.location.href="${to}"</script>`)
+      stream.injectToStream(`<script>window.location.href="${url}"</script>`)
     }
 
-    throw redirect(to)
+    throw redirect(url, options?.statusCode)
   }
 
   return navigateFn

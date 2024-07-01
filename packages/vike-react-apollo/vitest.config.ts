de6449e47eaffb22d48e0612b6559991b1bf9251
Dániel Horváth <horvath.daniel@lexunit.hu>
2024-07-01 02:28:01 +0200

    fix test

--- packages/vike-react-apollo/vitest.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vitest/config'
-
-export default defineConfig({
-  test: {
-    // test/**/*.test.ts => @brillout/test-e2e
-    include: ['**/*.spec.*'],
-    environment: 'jsdom'
-  }
-})

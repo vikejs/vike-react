7f5ac0de062fcccc13d99a8e3d1fe2d546e1e974
Dániel Horváth <horvath.daniel@lexunit.hu>
2024-07-01 19:25:37 +0200

    add clientOnly

--- packages/vike-react/src/components/ClientOnly.tsx
+++ packages/vike-react/src/components/ClientOnly.tsx
@@ -1,7 +1,8 @@
 export { ClientOnly }
+export { clientOnly }
 
-import React, { lazy, useEffect, useState, startTransition } from 'react'
-import type { ComponentType, ReactNode } from 'react'
+import React, { lazy, useEffect, useState, startTransition, Suspense } from 'react'
+import type { ComponentProps, ComponentType, ReactNode } from 'react'
 
 function ClientOnly<T>({
   load,
@@ -39,4 +40,37 @@ function ClientOnly<T>({
   }, deps)
 
   return Component ? <Component /> : fallback
+}
+
+//@ts-expect-error
+import.meta.env ??= { SSR: true }
+
+function clientOnly<T extends ComponentType<any>>(load: () => Promise<{ default: T }>) {
+  if (import.meta.env.SSR) {
+    return (props: ComponentProps<T> & { fallback?: JSX.Element }) => props.fallback
+  }
+
+  const Component = lazy(load)
+
+  return (props: ComponentProps<T> & { fallback?: JSX.Element }) => {
+    const [mounted, setMounted] = useState(false)
+
+    useEffect(() => {
+      setMounted(true)
+    }, [])
+
+    if (!mounted) {
+      return props.fallback
+    }
+
+    const { fallback, ...rest } = props
+    return (
+      <Suspense fallback={props.fallback}>
+        {
+          //@ts-ignore
+          <Component {...rest} />
+        }
+      </Suspense>
+    )
+  }
 }

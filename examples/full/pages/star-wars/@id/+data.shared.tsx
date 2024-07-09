2c4f0282ad70f091a2f57989bf5d84e328115af0
Romuald Brillout <git@brillout.com>
2024-07-09 23:47:31 +0200

    minor refactor

--- examples/full/pages/star-wars/@id/+data.shared.tsx
+++ examples/full/pages/star-wars/@id/+data.shared.tsx
@@ -13,7 +13,7 @@ const data = async (pageContext: PageContextServer) => {
   const response = await fetch(`https://brillout.github.io/star-wars/api/films/${pageContext.routeParams.id}.json`)
   let movie = (await response.json()) as MovieDetails
 
-  const title = movie.title
+  const { title } = movie
   config({
     title, // <title>
     head: (

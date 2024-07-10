239e4b097dc7f0ef32cc1b13b9d4c6938f08c908
Romuald Brillout <git@brillout.com>
2024-07-10 21:03:43 +0200

    Revert "factor out isomorphic useConfig() implementation"
    This reverts commit b3fab527ad6c4d5426e8d267220abbfc0e72217c.

--- packages/vike-react/src/hooks/useConfig/useConfig-client.ts
+++ packages/vike-react/src/hooks/useConfig/useConfig-client.ts
@@ -1,13 +1,36 @@
 export { useConfig }
 
 import type { ConfigFromHook } from '../../types/Config.js'
-import { useConfigShared, type ConfigSetter } from './shared.js'
+import type { PageContextInternal } from '../../types/PageContext.js'
+import { getPageContext } from 'vike/getPageContext'
+import { usePageContext } from '../usePageContext.js'
 
-function useConfig(): ConfigSetter {
-  return useConfigShared(sideEffect)
-}
+function useConfig(): (config: ConfigFromHook) => void {
+  const setUsingPageContext = (config: ConfigFromHook) => {
+    pageContext._configFromHook ??= {}
+    Object.assign(pageContext._configFromHook, config)
+  }
 
-function sideEffect(config: ConfigFromHook) {
-  const { title } = config
-  if (title) window.document.title = title
+  // getPageContext() enables useConfig() to be used for Vike hooks
+  let pageContext = getPageContext() as PageContextInternal
+  if (pageContext) return setUsingPageContext
+
+  // usePageContext() enables useConfig() to be used as React hook for React components
+  pageContext = usePageContext()
+  return (config: ConfigFromHook) => {
+    const headAlreadySet = '_headAlreadySet' in pageContext && pageContext._headAlreadySet
+
+    // No need to use injectToStream()
+    if (!headAlreadySet) {
+      setUsingPageContext(config)
+      return
+    }
+
+    const { title } = config
+    if (title) window.document.title = title
+
+    // Add support for following?
+    // - favicon
+    // - lang
+  }
 }

4829ad12615a3194d97b4456d462674cfd15cb0f
Romuald Brillout <git@brillout.com>
2024-07-15 12:11:19 +0200

    minor refactor

--- packages/vike-react/src/renderer/ssrEffect.ts
+++ packages/vike-react/src/renderer/ssrEffect.ts
@@ -4,18 +4,17 @@ import type { ConfigEffect } from 'vike/types'
 
 function ssrEffect({ configDefinedAt, configValue }: Parameters<ConfigEffect>[0]): ReturnType<ConfigEffect> {
   if (typeof configValue !== 'boolean') throw new Error(`${configDefinedAt} should be a boolean`)
+  const env = {
+    // Always load on the client-side.
+    client: true,
+    // When the SSR flag is false, we want to render the page only on the client-side.
+    // We achieve this by loading `Page` only on the client-side: when onRenderHtml()
+    // gets a `Page` value that is undefined it skip server-side rendering.
+    server: configValue !== false
+  }
   return {
     meta: {
-      Page: {
-        env: {
-          // Always load `Page` on the client-side.
-          client: true,
-          // When the SSR flag is false, we want to render the page only on the client-side.
-          // We achieve this by loading `Page` only on the client-side: when onRenderHtml()
-          // gets a `Page` value that is undefined it skip server-side rendering.
-          server: configValue !== false
-        }
-      }
+      Page: { env }
     }
   }
 }

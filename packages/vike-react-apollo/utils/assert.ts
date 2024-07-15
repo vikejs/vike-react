4c7021525e09e3737acfb598be3b10af932c48e4
Romuald Brillout <git@brillout.com>
2024-07-15 11:57:52 +0200

    minor

--- packages/vike-react-apollo/utils/assert.ts
+++ packages/vike-react-apollo/utils/assert.ts
@@ -1,9 +1,4 @@
-export { assert, assertUsage }
-
-function assert(condition: unknown): asserts condition {
-  if (condition) return
-  throw new Error("You stumbled upon a bug in vike-react-apollo's source code, reach out on GitHub.")
-}
+export { assertUsage }
 
 function assertUsage(condition: unknown, message: string): asserts condition {
   if (condition) return

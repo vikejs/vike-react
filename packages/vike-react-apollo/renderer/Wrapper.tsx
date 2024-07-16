95bc323c696091bae908c72e38a010f27eff22e0
nitedani <67418286+nitedani@users.noreply.github.com>
2024-07-16 21:47:03 +0200

    feat: add vike-react-apollo (#134)

---
+++ packages/vike-react-apollo/renderer/Wrapper.tsx
@@ -0,0 +1,11 @@
+import React, { type ReactNode } from 'react'
+import { usePageContext } from 'vike-react/usePageContext'
+import { assertUsage } from '../utils/assert.js'
+import { WrappedApolloProvider } from './Transport.js'
+
+export default function Wrapper({ children }: { children: ReactNode }) {
+  const pageContext = usePageContext()
+  const { ApolloClient: getApolloClient } = pageContext.config
+  assertUsage(getApolloClient, 'Setting +ApolloClient is required')
+  return <WrappedApolloProvider makeClient={() => getApolloClient(pageContext)}>{children}</WrappedApolloProvider>
+}

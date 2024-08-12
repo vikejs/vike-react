ded88e14aba0d71f7bf2218eaf113d41ecf9598e
Romuald Brillout <git@brillout.com>
2024-08-12 07:50:12 +0200

    refactor useConfig-server.ts

--- packages/vike-react/src/hooks/useConfig/useConfig-server.ts
+++ packages/vike-react/src/hooks/useConfig/useConfig-server.ts
@@ -6,6 +6,8 @@ import type { ConfigFromHook } from '../../types/Config.js'
 import { usePageContext } from '../usePageContext.js'
 import { getPageContext } from 'vike/getPageContext'
 import { useStream } from 'react-streaming'
+import { objectKeys } from '../../utils/objectKeys.js'
+import { includes } from '../../utils/includes.js'
 
 /**
  * Set configurations inside React components and Vike hooks.
@@ -13,54 +15,48 @@ import { useStream } from 'react-streaming'
  * https://vike.dev/useConfig
  */
 function useConfig(): (config: ConfigFromHook) => void {
-  const configSetter = (config: ConfigFromHook) => setConfigOverPageContext(config, pageContext)
-
   // Vike hook
   let pageContext = getPageContext() as PageContext & PageContextInternal
-  if (pageContext) return configSetter
+  if (pageContext) return (config: ConfigFromHook) => setPageContextConfigFromHook(config, pageContext)
 
   // React component
   pageContext = usePageContext()
   const stream = useStream()
   return (config: ConfigFromHook) => {
     if (!pageContext._headAlreadySet) {
-      configSetter(config)
+      setPageContextConfigFromHook(config, pageContext)
     } else {
       // <head> already sent to the browser => send DOM-manipulating scripts during HTML streaming
-      sideEffect(config, stream!)
+      apply(config, stream!)
     }
   }
 }
 
-const configsHtmlOnly = ['Head', 'description', 'image'] as const
+const configsClientSide = ['title']
 const configsCumulative = ['Head'] as const
-const configsOverridable = ['title', 'description', 'image'] as const
-function setConfigOverPageContext(config: ConfigFromHook, pageContext: PageContext & PageContextInternal) {
+function setPageContextConfigFromHook(config: ConfigFromHook, pageContext: PageContext & PageContextInternal) {
   pageContext._configFromHook ??= {}
+  objectKeys(config).forEach((configName) => {
+    // Skip HTML only configs which the client-side doesn't need, saving KBs sent to the client as well as avoiding serialization errors.
+    if (pageContext.isClientSideNavigation && !configsClientSide.includes(configName)) return
 
-  if (pageContext.isClientSideNavigation) {
-    // Remove HTML only configs which the client-side doesn't need (also avoiding serialization errors)
-    for (const configName of configsHtmlOnly) delete config[configName]
-  }
-
-  // Cumulative values
-  configsCumulative.forEach((configName) => {
-    const configValue = config[configName]
-    if (!configValue) return
-    pageContext._configFromHook![configName] ??= []
-    pageContext._configFromHook![configName].push(configValue)
-  })
-
-  // Overridable values
-  configsOverridable.forEach((configName) => {
-    const configValue = config[configName]
-    if (!configValue) return
-    pageContext._configFromHook![configName] = configValue
+    if (!includes(configsCumulative, configName)) {
+      // Overridable config
+      const configValue = config[configName]
+      if (!configValue) return // TODO: only skip `undefined`
+      pageContext._configFromHook![configName] = configValue
+    } else {
+      // Cumulative config
+      const configValue = config[configName]
+      if (!configValue) return
+      pageContext._configFromHook![configName] ??= []
+      pageContext._configFromHook![configName].push(configValue)
+    }
   })
 }
 
 type Stream = NonNullable<ReturnType<typeof useStream>>
-function sideEffect(config: ConfigFromHook, stream: Stream) {
+function apply(config: ConfigFromHook, stream: Stream) {
   const { title } = config
   if (title) {
     const htmlSnippet = `<script>document.title = ${JSON.stringify(title)}</script>`

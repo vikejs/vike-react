a6c82a27ec5a2e916a1fdc93dae5a9465286f93b
Romuald Brillout <git@brillout.com>
2024-08-15 14:13:14 +0200

    fix: fix callable cumulative configs

--- packages/vike-react/src/renderer/getHeadSetting.tsx
+++ packages/vike-react/src/renderer/getHeadSetting.tsx
@@ -4,22 +4,29 @@ import { isCallable } from '../utils/isCallable.js'
 import type { PageContext } from 'vike/types'
 import type { PageContextInternal } from '../types/PageContext.js'
 import type { ConfigFromHookResolved } from '../types/Config.js'
+import { configsCumulative } from '../hooks/useConfig/configsCumulative.js'
+import { includes } from '../utils/includes.js'
 
-// We use `any` instead of doing proper validation in order to save KBs sent to the client-side
+// We use `any` instead of doing proper validation in order to save KBs sent to the client-side.
 
 function getHeadSetting<T>(
   configName: keyof ConfigFromHookResolved,
   pageContext: PageContext & PageContextInternal
 ): undefined | T {
   // Set by useConfig()
-  {
-    const val = pageContext._configFromHook?.[configName]
-    if (val !== undefined) return val as any
-  }
-
+  const valFromUseConfig = pageContext._configFromHook?.[configName]
   // Set by +configName.js
+  const valFromConfig = pageContext.config[configName]
+
   const getCallable = (val: unknown) => (isCallable(val) ? val(pageContext) : val)
-  const val = pageContext.config[configName]
-  if (Array.isArray(val)) return val.map(getCallable) as any // cumulative configs
-  return getCallable(val) as any
+  if (!includes(configsCumulative, configName)) {
+    if (valFromUseConfig !== undefined) return valFromUseConfig as any
+    return getCallable(valFromConfig) as any
+  } else {
+    return [
+      //
+      ...((valFromConfig as any) ?? []).map(getCallable),
+      ...((valFromUseConfig as any) ?? [])
+    ] as any
+  }
 }

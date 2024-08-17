b61c80edefbb0bdd551feccce0d18b58ce241b26
Romuald Brillout <git@brillout.com>
2024-08-17 11:29:42 +0200

    minor refactor

--- packages/vike-react/src/components/Config/Config-client.ts
+++ packages/vike-react/src/components/Config/Config-client.ts
@@ -1,15 +1,8 @@
 export { Config }
 
-// Same as ./Config-server.ts but importing useConfig-client.js
-
 import { useConfig } from '../../hooks/useConfig/useConfig-client.js'
 import type { ConfigFromHook } from '../../types/Config.js'
 
-/**
- * Set configurations inside React components.
- *
- * https://vike.dev/useConfig
- */
 function Config(props: ConfigFromHook): null {
   const config = useConfig()
   config(props)
--- packages/vike-react/src/components/Config/Config-server.ts
+++ packages/vike-react/src/components/Config/Config-server.ts
@@ -1,7 +1,5 @@
 export { Config }
 
-// Same as ./Config-client.ts but importing useConfig-server.js
-
 import { useConfig } from '../../hooks/useConfig/useConfig-server.js'
 import type { ConfigFromHook } from '../../types/Config.js'
 

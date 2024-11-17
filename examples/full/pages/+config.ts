fd91ba8ccd9187db8950772d19af3a364c46c97e
Romuald Brillout <git@brillout.com>
2024-11-17 18:42:01 +0100

    Revert "reproduce bug"
    This reverts commit e6fb7ee6510cf984df9b229176385fed44cc1639.

--- examples/full/pages/+config.ts
+++ examples/full/pages/+config.ts
@@ -2,7 +2,6 @@ export { config }
 
 import type { Config } from 'vike/types'
 import vikeReact from 'vike-react/config'
-import vikeReactChakra from 'vike-react-chakra/config'
 
 // Default configs (can be overridden by pages)
 const config = {
@@ -15,5 +14,5 @@ const config = {
   bodyAttributes: { class: 'dark' },
   viewport: 999,
   // https://vike.dev/extends
-  extends: [vikeReact, vikeReactChakra],
+  extends: vikeReact,
 } satisfies Config

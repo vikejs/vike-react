fcac4b0ac9076925f6a874d05fd6129fd7c787ac
Romuald Brillout <git@brillout.com>
2024-11-28 13:43:01 +0100

    fix: rename +config.js to config.js

--- packages/vike-react/src/index.ts
+++ packages/vike-react/src/index.ts
@@ -2,4 +2,4 @@
 console.warn(
   "[vike-react][warning][deprecation] Replace `import vikeReact from 'vike-react'` with `import vikeReact from 'vike-react/config'` (typically in your /pages/+config.js)",
 )
-export { default } from './+config.js'
+export { default } from './config.js'

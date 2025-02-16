b45bedd151508b9a916381da66cacab63fa44746
Romuald Brillout <git@brillout.com>
2025-02-16 13:41:10 +0100

    tmp2

--- packages/vike-react/src/utils/isCallable.ts
+++ packages/vike-react/src/utils/isCallable.ts
@@ -1,3 +1,3 @@
-export function isCallable<T extends (...args: any[]) => any>(thing: T | unknown): thing is T {
+export function isCallable<T extends (...args: unknown[]) => unknown>(thing: T | unknown): thing is T {
   return thing instanceof Function || typeof thing === 'function'
 }

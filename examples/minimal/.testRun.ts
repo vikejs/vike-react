69602f0ce8829d3f15751635b27ebcd5fd1d4153
Romuald Brillout <git@brillout.com>
2025-04-08 13:40:35 +0200

    add examples/minimal/ test

---
+++ examples/minimal/.testRun.ts
@@ -0,0 +1,49 @@
+export { testRunClassic as testRun }
+
+import { test, expect, run, fetchHtml, page, getServerUrl, autoRetry } from '@brillout/test-e2e'
+
+function testRunClassic(cmd: 'npm run dev' | 'npm run preview' | 'npm run prod') {
+  run(cmd)
+
+  test('page content is rendered to HTML', async () => {
+    const html = await fetchHtml('/')
+    expect(html).toContain('<h1>Welcome</h1>')
+  })
+
+  test('page is rendered to the DOM and interactive', async () => {
+    await page.goto(getServerUrl() + '/')
+    await page.click('a[href="/"]')
+    expect(await page.textContent('h1')).toBe('Welcome')
+    await testCounter()
+  })
+
+  test('about page', async () => {
+    await page.click('a[href="/about"]')
+    await autoRetry(async () => {
+      expect(await page.textContent('h1')).toBe('About')
+    })
+    expect(await page.textContent('p')).toBe('Example of using Vike.')
+    const html = await fetchHtml('/about')
+    expect(html).toContain('<h1>About</h1>')
+  })
+}
+
+async function testCounter(currentValue = 0) {
+  // autoRetry() in case page just got client-side navigated
+  await autoRetry(
+    async () => {
+      const btn = page.locator('button', { hasText: 'Counter' })
+      expect(await btn.textContent()).toBe(`Counter ${currentValue}`)
+    },
+    { timeout: 5 * 1000 },
+  )
+  // autoRetry() in case page isn't hydrated yet
+  await autoRetry(
+    async () => {
+      const btn = page.locator('button', { hasText: 'Counter' })
+      await btn.click()
+      expect(await btn.textContent()).toBe(`Counter ${currentValue + 1}`)
+    },
+    { timeout: 5 * 1000 },
+  )
+}

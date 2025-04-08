0329c54e52c46c619fa199512a49424e81042235
Romuald Brillout <git@brillout.com>
2025-04-08 19:41:13 +0200

    simplify test

--- examples/redux/.testRun.ts
+++ examples/redux/.testRun.ts
@@ -5,34 +5,29 @@ import { test, expect, run, page, getServerUrl, autoRetry } from '@brillout/test
 function testRun(cmd: `pnpm run ${'dev' | 'preview'}`) {
   run(cmd)
 
-  const content1 = 'This page is'
-  const content2 = 'The counter value is'
-
   test('The store persists across page navigation', async () => {
-    let body: string | null
-
     await page.goto(getServerUrl() + '/')
-    body = await page.textContent('body')
-
-    expect(body).toContain(content1)
     await testCounter()
-
     await page.click('a:has-text("About")')
-    await page.click('button')
-
-    body = await page.textContent('body')
-    expect(body).toContain(content2)
-    expect(await page.textContent('button')).toContain('Counter 2')
+    await testCounter(1)
   })
 }
 
-async function testCounter() {
-  // autoRetry() for awaiting client-side code loading & executing
+async function testCounter(currentValue = 0) {
+  // autoRetry() in case page just got client-side navigated
   await autoRetry(
     async () => {
-      expect(await page.textContent('button')).toBe('Counter 0')
-      await page.click('button')
-      expect(await page.textContent('button')).toContain('Counter 1')
+      const btn = page.locator('button', { hasText: 'Counter' })
+      expect(await btn.textContent()).toBe(`Counter ${currentValue}`)
+    },
+    { timeout: 5 * 1000 },
+  )
+  // autoRetry() in case page isn't hydrated yet
+  await autoRetry(
+    async () => {
+      const btn = page.locator('button', { hasText: 'Counter' })
+      await btn.click()
+      expect(await btn.textContent()).toBe(`Counter ${currentValue + 1}`)
     },
     { timeout: 5 * 1000 },
   )

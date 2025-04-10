1f83f4c76c848b9a74d9c857b9afab54bc255266
Romuald Brillout <git@brillout.com>
2025-04-10 20:36:41 +0200

    Revert "small refactor"
    This reverts commit ee97e7fb019838fb31518a98ccea499bfac22141.

--- packages/vike-react-redux/onBeforeRenderClient.ts
+++ packages/vike-react-redux/onBeforeRenderClient.ts
@@ -1,11 +1,9 @@
 export { onBeforeRenderClient }
 
 import type { PageContextClient } from 'vike/types'
-import { assert } from './utils/assert'
 
 function onBeforeRenderClient(pageContext: PageContextClient) {
   const createStore = pageContext.config.redux?.createStore
   if (!createStore) return
-  if (!pageContext.isHydration) assert(pageContext.reduxStore)
   pageContext.reduxStore = createStore(pageContext.reduxState)
 }
--- packages/vike-react-redux/utils/assert.ts
+++
@@ -1,4 +0,0 @@
-export function assert(condition: unknown): asserts condition {
-  if (condition) return
-  throw new Error('You stumbled upon a vike-react-redux bug, reach out on GitHub.')
-}

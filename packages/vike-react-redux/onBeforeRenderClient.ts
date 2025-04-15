1bdfe7706598f535dc0570589f5e2110fa8ac696
Romuald Brillout <git@brillout.com>
2025-04-15 09:39:22 +0200

    Revert "small refactor (2)"
    This reverts commit 5eb92224cb09833fd0d0657f454a05698c077c00.

--- packages/vike-react-redux/onBeforeRenderClient.ts
+++ packages/vike-react-redux/onBeforeRenderClient.ts
@@ -5,7 +5,5 @@ import type { PageContextClient } from 'vike/types'
 function onBeforeRenderClient(pageContext: PageContextClient) {
   const createStore = pageContext.config.redux?.createStore
   if (!createStore) return
-  if (pageContext.isHydration) {
-    pageContext.reduxStore = createStore(pageContext.reduxState)
-  }
+  pageContext.reduxStore = createStore(pageContext.reduxState)
 }

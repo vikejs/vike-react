45fcd43263a1446f0fea067126b29b18622df6f6
Romuald Brillout <git@brillout.com>
2025-04-19 12:16:14 +0200

    assert

--- packages/vike-react-redux/onAfterRenderHtml.server.ts
+++ packages/vike-react-redux/onAfterRenderHtml.server.ts
@@ -1,8 +1,10 @@
 export { onAfterRenderHtml }
 
 import type { PageContextServer } from 'vike/types'
+import { assert } from './utils/assert'
 
 function onAfterRenderHtml(pageContext: PageContextServer) {
   if (!pageContext.redux) return
   pageContext.redux.ssrState = pageContext.redux.store.getState()
+  assert(pageContext.redux.ssrState)
 }

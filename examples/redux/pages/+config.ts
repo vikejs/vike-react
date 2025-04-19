3828cbae560fb19c9197202b689bf3c00f28efa0
Romuald Brillout <git@brillout.com>
2025-04-19 12:37:29 +0200

    improve test

--- examples/redux/pages/+config.ts
+++ examples/redux/pages/+config.ts
@@ -4,6 +4,7 @@ import vikeReactRedux from 'vike-react-redux/config'
 import type { Config } from 'vike/types'
 
 export default {
+  prerender: true,
   Layout,
   extends: [vikeReact, vikeReactRedux],
 } satisfies Config

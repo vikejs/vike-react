4cac6bbe9d2209188d8133b8bd5e2b239d02a099
Horváth Dániel <nitedani@gmail.com>
2025-04-19 14:41:08 +0200

    format

--- packages/vike-react-zustand/src/integration/+config.ts
+++ packages/vike-react-zustand/src/integration/+config.ts
@@ -1,4 +1,4 @@
-import 'vike-react/config'; // Needed for declaration merging of Config
+import 'vike-react/config' // Needed for declaration merging of Config
 import type { Config } from 'vike/types'
 import { vikeReactZustand } from '../plugin/index.js'
 

3ddbb9b1e07dd0da926b249efcbd1ba4e8dc6bc1
Romuald Brillout <git@brillout.com>
2025-04-19 10:18:20 +0200

    fix

--- packages/vike-react-redux/onBeforeRenderClient.ts
+++ packages/vike-react-redux/onBeforeRenderClient.ts
@@ -5,5 +5,5 @@ import type { PageContextClient } from 'vike/types'
 function onBeforeRenderClient(pageContext: PageContextClient) {
   const createStore = pageContext.config.redux?.createStore
   if (!createStore) return
-  pageContext.globalContext.reduxStore = createStore(pageContext.reduxState)
+  pageContext.globalContext.reduxStore ??= createStore(pageContext.reduxState)
 }

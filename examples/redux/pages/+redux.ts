61e4641be5de1eccfe1139c251b54445717b7eaf
Romuald Brillout <git@brillout.com>
2025-04-19 10:44:36 +0200

    minor refactor

--- examples/redux/pages/+redux.ts
+++ examples/redux/pages/+redux.ts
@@ -1,16 +1,2 @@
+import { createStore } from '../store/createStore'
 export default { createStore }
-export type AppStore = ReturnType<typeof createStore>
-export type RootState = ReturnType<AppStore['getState']>
-export type AppDispatch = AppStore['dispatch']
-
-import { combineReducers, configureStore } from '@reduxjs/toolkit'
-import { counterReducer } from '../store/counter/counterSlice'
-import type { PageContext } from 'vike/types'
-const rootReducer = combineReducers({ counter: counterReducer })
-
-function createStore(pageContext: PageContext) {
-  return configureStore({
-    reducer: rootReducer,
-    preloadedState: pageContext.reduxState,
-  })
-}
--- examples/redux/store/hooks.ts
+++ examples/redux/store/hooks.ts
@@ -1,6 +1,6 @@
 // This file serves as a central hub for re-exporting pre-typed Redux hooks.
 import { useDispatch, useSelector, useStore } from 'react-redux'
-import type { AppDispatch, AppStore, RootState } from '../pages/+redux'
+import type { AppDispatch, AppStore, RootState } from './createStore'
 
 // Use throughout your app instead of plain `useDispatch` and `useSelector`
 export const useAppDispatch = useDispatch.withTypes<AppDispatch>()

cbeabbfe6742651280613dfcc31cebcce3ed37b4
Romuald Brillout <git@brillout.com>
2025-04-19 10:55:33 +0200

    minor refactor

--- packages/vike-react-redux/config.ts
+++ packages/vike-react-redux/config.ts
@@ -9,17 +9,20 @@ const config = {
     vike: '>=0.4.211',
     'vike-react': '>=0.4.13',
   },
+
   passToClient: ['reduxState'],
-  onAfterRenderHtml: 'import:vike-react-redux/__internal/onAfterRenderHtml:onAfterRenderHtml',
-  onCreatePageContext: 'import:vike-react-redux/__internal/onCreatePageContext:onCreatePageContext',
-  onBeforeRenderClient: 'import:vike-react-redux/__internal/onBeforeRenderClient:onBeforeRenderClient',
-  Wrapper: 'import:vike-react-redux/__internal/Wrapper:Wrapper',
+
   meta: {
     redux: {
       env: { server: true, client: true },
       global: true,
     },
   },
+
+  onAfterRenderHtml: 'import:vike-react-redux/__internal/onAfterRenderHtml:onAfterRenderHtml',
+  onCreatePageContext: 'import:vike-react-redux/__internal/onCreatePageContext:onCreatePageContext',
+  onBeforeRenderClient: 'import:vike-react-redux/__internal/onBeforeRenderClient:onBeforeRenderClient',
+  Wrapper: 'import:vike-react-redux/__internal/Wrapper:Wrapper',
 } satisfies Config
 
 declare global {

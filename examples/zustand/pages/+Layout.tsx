6d0d570be8b2b32b363e07ed916492e8c4ecf8e4
Romuald Brillout <git@brillout.com>
2025-04-26 13:40:32 +0200

    add /about page

--- examples/zustand/pages/+Layout.tsx
+++ examples/zustand/pages/+Layout.tsx
@@ -4,8 +4,7 @@ export const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div id="page-content">
       <nav>
-        <a href="/">Home</a>
-        <a href="/about">About</a>
+        <a href="/">Home</a> <a href="/about">About</a>
       </nav>
       {children}
     </div>

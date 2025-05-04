4d5e37c4caa56d41144666d43001f5d369652086
Romuald Brillout <git@brillout.com>
2025-05-04 22:10:08 +0200

    minor refactor

--- examples/redux/pages/index/+data.ts
+++ examples/redux/pages/index/+data.ts
@@ -6,15 +6,15 @@ import { fetchCountInit } from '../../components/Counter/fetchCountInit'
 import type { PageContextServer } from 'vike/types'
 
 async function data(pageContext: PageContextServer) {
-  const countInit = await fetchCountInit()
-  const todosInit = await fetchTodosInit()
-  return {
-    countInit,
-    todosInit,
-  }
+  const [countInit, todosInit] = await Promise.all([fetchCountInit(), fetchTodosInit()])
+  return { countInit, todosInit }
 }
 
 // Pretending the list is fetched over the network
 async function fetchTodosInit() {
-  return [{ text: 'Buy apples' }, { text: `Update Node.js ${process.version} to latest version` }]
+  return [
+    //
+    { text: 'Buy apples' },
+    { text: `Update Node.js ${process.version} to latest version` },
+  ]
 }

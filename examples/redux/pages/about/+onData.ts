000989e4e52da04833f4129257fb25c992a9d1cb
Romuald Brillout <git@brillout.com>
2025-05-16 12:39:26 +0200

    use globalContext fallback

--- examples/redux/pages/about/+onData.ts
+++ examples/redux/pages/about/+onData.ts
@@ -6,7 +6,7 @@ import type { Data } from './+data'
 import { initializeCount } from '../../store/slices/count'
 
 function onData(pageContext: PageContext & { data: Data }) {
-  const store = !pageContext.isClientSide ? pageContext.store : pageContext.globalContext.store
+  const { store } = pageContext
   store.dispatch(initializeCount(pageContext.data.countInit))
   // Save KBs: we don't need to pass the data to the client-side
   if (!pageContext.isClientSide && !pageContext.isPrerendering) delete (pageContext as { data?: Data }).data
--- examples/redux/pages/index/+onData.ts
+++ examples/redux/pages/index/+onData.ts
@@ -7,7 +7,7 @@ import { initializeCount } from '../../store/slices/count'
 import { initializeTodos } from '../../store/slices/todos'
 
 function onData(pageContext: PageContext & { data: Data }) {
-  const store = !pageContext.isClientSide ? pageContext.store : pageContext.globalContext.store
+  const { store } = pageContext
   store.dispatch(initializeTodos(pageContext.data.todosInit))
   store.dispatch(initializeCount(pageContext.data.countInit))
   // Save KBs: we don't need to pass the data to the client-side

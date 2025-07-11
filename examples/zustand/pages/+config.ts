63950323133ddfac47e418c82e29490ee6efce4a
Romuald Brillout <git@brillout.com>
2025-07-11 19:33:36 +0200

    fix: automatically enable +stream (fix vikejs/vike#2549)

--- examples/zustand/pages/+config.ts
+++ examples/zustand/pages/+config.ts
@@ -4,14 +4,7 @@ import type { Config } from 'vike/types'
 import vikeReact from 'vike-react/config'
 import vikeReactZustand from 'vike-react-zustand/config'
 
-// Default configs (can be overridden by pages)
 const config = {
-  // <title>
   title: 'My Vike + React App',
-  // https://vike.dev/stream
-  stream: true,
-  // https://vike.dev/ssr - this line can be removed since `true` is the default
-  ssr: true,
-  // https://vike.dev/extends
   extends: [vikeReact, vikeReactZustand],
 } satisfies Config
--- packages/vike-react-zustand/src/integration/config.ts
+++ packages/vike-react-zustand/src/integration/config.ts
@@ -1,11 +1,13 @@
 import type { Config } from 'vike/types'
 import { vikeReactZustand } from '../plugin/index.js'
+import 'vike-react/config' // Needed for merging vike-react's Vike.Config such as +stream
 
 export default {
   name: 'vike-react-zustand',
   require: {
     'vike-react': '>=0.4.13',
   },
+  stream: { require: true },
   vite: {
     plugins: [vikeReactZustand()],
   },

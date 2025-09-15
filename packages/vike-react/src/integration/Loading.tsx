aec0ab6ee48697cf2abd238e3f6afe0535be6ad4
Teddy Bradford <3684553+teddybradford@users.noreply.github.com>
2025-09-14 21:50:43 -0400

    refactor Loading integration: inline keyframe styles

--- packages/vike-react/src/integration/Loading.tsx
+++ packages/vike-react/src/integration/Loading.tsx
@@ -3,25 +3,31 @@ export default {
 }
 
 import React from 'react'
-/* We can't import it here: https://github.com/vikejs/vike/issues/2460
- * - We import it inside onRenderClient.js instead.
- * - We'll be able to do it if Vite + Rolldown always transpiles the server-side.
-import './Loading.css'
-*/
 
 function LoadingComponent() {
   return (
-    <div
-      style={{
-        width: '100%',
-        height: '100%',
-        maxHeight: '100%',
-        background: 'linear-gradient(110deg, #ececec 8%, #f5f5f5 18%, #ececec 33%)',
-        borderRadius: '5px',
-        backgroundSize: '200% 100%',
-        animation: '1.3s vike-react-shine linear infinite',
-        aspectRatio: '2.5/1',
-      }}
-    />
+    <>
+      <style href="VikeReactLoading" precedence="low">
+        {`
+          @keyframes vike-react-loading {
+            to {
+              background-position-x: -200%;
+            }
+          }
+        `}
+      </style>
+      <div
+        style={{
+          width: '100%',
+          height: '100%',
+          maxHeight: '100%',
+          background: 'linear-gradient(110deg, #ececec 8%, #f5f5f5 18%, #ececec 33%)',
+          borderRadius: '5px',
+          backgroundSize: '200% 100%',
+          animation: '1.3s vike-react-loading linear infinite',
+          aspectRatio: '2.5/1',
+        }}
+      />
+    </>
   )
 }

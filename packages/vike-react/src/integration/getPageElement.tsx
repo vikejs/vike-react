12991c216f57be7efa446498e50af1d0adf48309
Romuald Brillout <git@brillout.com>
2025-09-16 10:33:43 +0200

    failed attempt use error boundary

--- packages/vike-react/src/integration/getPageElement.tsx
+++ packages/vike-react/src/integration/getPageElement.tsx
@@ -1,10 +1,10 @@
 export { getPageElement }
 
-import React, { Suspense, useEffect } from 'react'
+import React, { Suspense, useEffect, useState, Component } from 'react'
 import type { PageContext } from 'vike/types'
 import { PageContextProvider } from '../hooks/usePageContext.js'
 
-function getPageElement(pageContext: PageContext): { page: React.JSX.Element; renderPromise: Promise<void> } {
+function getPageElement(pageContext: PageContext) {
   const {
     Page,
     config: { Loading },
@@ -30,8 +30,18 @@ function getPageElement(pageContext: PageContext): { page: React.JSX.Element; re
   page = <PageContextProvider pageContext={pageContext}>{page}</PageContextProvider>
 
   let renderPromiseResolve!: () => void
-  let renderPromise = new Promise<void>((r) => (renderPromiseResolve = r))
-  page = <RenderPromiseProvider renderPromiseResolve={renderPromiseResolve}>{page}</RenderPromiseProvider>
+  let renderPromiseReject!: (err: unknown) => void
+  let renderPromise = new Promise<void>((resolve, reject) => {
+    renderPromiseResolve = resolve
+    renderPromiseReject = reject
+  })
+  const renderKey = Math.random()
+  // @ts-ignore
+  page = (
+    <VikeReactErrorBoundary renderKey={renderKey} onError={renderPromiseReject}>
+      <RenderPromiseProvider renderPromiseResolve={renderPromiseResolve}>{page}</RenderPromiseProvider>
+    </VikeReactErrorBoundary>
+  )
 
   if (pageContext.config.reactStrictMode !== false) {
     page = <React.StrictMode>{page}</React.StrictMode>
@@ -40,10 +50,56 @@ function getPageElement(pageContext: PageContext): { page: React.JSX.Element; re
   return { page, renderPromise }
 }
 
+// TODO/now rename
 function RenderPromiseProvider({
   children,
   renderPromiseResolve,
 }: { children: React.ReactNode; renderPromiseResolve: () => void }) {
   useEffect(renderPromiseResolve)
   return children
+}
+
+class VikeReactErrorBoundary extends Component {
+  state = { hasError: null }
+
+  static getDerivedStateFromError() {
+    console.log('getDerivedStateFromError()')
+    // @ts-ignore
+    //this.props.onError(error)
+    return { hasError: this.props.renderKey }
+  }
+
+  componentDidCatch(error: unknown, info: any) {
+    console.log('componentDidCatch()')
+    /*
+    console.error(
+      'EEEE',
+      error,
+      // Example "componentStack":
+      //   in ComponentThatThrows (created by App)
+      //   in ErrorBoundary (created by App)
+      //   in div (created by App)
+      //   in App
+      info.componentStack,
+      // Warning: `captureOwnerStack` is not available in production.
+      React.captureOwnerStack(),
+    );
+    */
+    // @ts-ignore
+    this.props.onError(error)
+  }
+
+  render() {
+    console.log('render')
+    //*
+    // @ts-ignore
+    if (this.state.hasError === this.props.renderKey) {
+      console.log('render 1')
+      return <p>Couldn't render page.</p>
+    }
+    console.log('render 2')
+    //*/
+    // @ts-ignore
+    return this.props.children
+  }
 }

113fc6afd7f310e666bdcf7117ee708611123f03
Gyula Voros <gyulavoros87@gmail.com>
2025-09-24 11:49:12 +0200

    Hono streaming issue

---
+++ examples/full/server.ts
@@ -0,0 +1,11 @@
+import { Hono } from 'hono';
+import { apply } from 'vike-server/hono';
+import { serve } from 'vike-server/hono/serve';
+
+function startServer() {
+  const app = new Hono();
+  apply(app);
+  return serve(app, {});
+}
+
+export default startServer();

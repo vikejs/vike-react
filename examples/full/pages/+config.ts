59a89ec86b82110ed70b47cb0fdf68611fc6cacb
Rom <git@brillout.com>
2025-09-24 17:16:15 +0200

    reproduce hono streaming issue (2) (vikejs/vike-server#134) (#189)

--- examples/full/pages/+config.ts
+++ examples/full/pages/+config.ts
@@ -2,6 +2,7 @@ export { config }
 
 import type { Config } from 'vike/types'
 import vikeReact from 'vike-react/config'
+import vikeServer from 'vike-server/config'
 
 // Default configs (can be overridden by pages)
 const config = {
@@ -14,5 +15,7 @@ const config = {
   bodyAttributes: { class: 'dark' },
   viewport: 999,
   // https://vike.dev/extends
-  extends: vikeReact,
+  extends: [vikeReact, vikeServer],
+
+  server: 'server.ts',
 } satisfies Config

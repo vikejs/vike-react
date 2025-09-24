1332f2debffbafd07d201ff6f11fe785be3f6ee9
Romuald Brillout <git@brillout.com>
2025-09-24 13:03:09 +0200

    Revert "sleep"
    This reverts commit a834d9ba0f259f0f2d332e46a99cc9d895cbedf2.

--- examples/full/pages/star-wars/index/+data.ts
+++ examples/full/pages/star-wars/index/+data.ts
@@ -10,7 +10,6 @@ import image from '../../../assets/logo-new.svg'
 const data = async () => {
   const config = useConfig()
 
-  await sleep(3 * 1000)
   const response = await fetch('https://brillout.github.io/star-wars/api/films.json')
   const moviesData = (await response.json()) as MovieDetails[]
 
@@ -33,8 +32,4 @@ function minimize(movies: MovieDetails[]): Movie[] {
     const { title, release_date, id } = movie
     return { title, release_date, id }
   })
-}
-
-function sleep(milliseconds: number): Promise<void> {
-  return new Promise((r) => setTimeout(r, milliseconds))
 }

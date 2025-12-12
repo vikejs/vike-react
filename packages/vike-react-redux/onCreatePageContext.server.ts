c70fbad474f9baf206b2f9904e35267cc397c22f
Romuald Brillout <git@brillout.com>
2025-12-12 17:08:24 +0100

    wip

--- packages/vike-react-redux/onCreatePageContext.server.ts
+++ packages/vike-react-redux/onCreatePageContext.server.ts
@@ -3,6 +3,7 @@ export { onCreatePageContext }
 import type { PageContextServer } from 'vike/types'
 
 function onCreatePageContext(pageContext: PageContextServer) {
+  if (pageContext.isClientSideNavigation) return // only SSR
   const configRedux = pageContext.config.redux
   if (!configRedux) return
   pageContext.store = configRedux.createStore(pageContext)

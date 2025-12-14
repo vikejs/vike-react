5cdff58ec1c4cacacfd4ef6d51cc66e16d70d71d
Romuald Brillout <git@brillout.com>
2025-12-14 17:56:03 +0100

    Revert "wip"
    This reverts commit bc5b1410ad48396b5ed3baf0bb75a1fe1619a61f.

--- packages/vike-react-redux/onCreatePageContext.server.ts
+++ packages/vike-react-redux/onCreatePageContext.server.ts
@@ -3,7 +3,7 @@ export { onCreatePageContext }
 import type { PageContextServer } from 'vike/types'
 
 function onCreatePageContext(pageContext: PageContextServer) {
-  if (pageContext.isClientSideNavigation || !pageContext.Page) return // only SSR
+  if (pageContext.isClientSideNavigation) return // only SSR
   const configRedux = pageContext.config.redux
   if (!configRedux) return
   pageContext.store = configRedux.createStore(pageContext)

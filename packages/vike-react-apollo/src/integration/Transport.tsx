a97f3f24bb8689567572daf921c3789bd3933342
Romuald Brillout <git@brillout.com>
2025-12-18 15:43:12 +0100

    partial revert

--- packages/vike-react-apollo/src/integration/Transport.tsx
+++ packages/vike-react-apollo/src/integration/Transport.tsx
@@ -3,36 +3,33 @@ import { buildManualDataTransport } from '@apollo/client-react-streaming/manual-
 import { useStream } from 'react-streaming'
 import { renderToString } from 'react-dom/server'
 import React from 'react'
-import type { PageContextServer } from 'vike/types'
 import { assert } from '../utils/assert.js'
+import { usePageContext } from 'vike-react/usePageContext'
 
-export function createWrappedApolloProvider(pageContext: PageContextServer) {
-  return WrapApolloProvider(
-    buildManualDataTransport({
-      useInsertHtml() {
-        const stream = useStream()
-        if (!globalThis.__VIKE__IS_CLIENT) {
-          assert(!pageContext.isClientSide)
-          if (!stream) {
-            return () => {}
-          }
-          return (callback: () => React.ReactNode) => {
-            const nonce = pageContext.cspNonce
-            stream.injectToStream(
-              // https://github.com/apollographql/apollo-client-nextjs/issues/325
-              (async () => {
-                let html = renderToString(await callback())
-                if (nonce) {
-                  // No need to escape the injected HTML — see https://github.com/vikejs/vike/blob/36201ddad5f5b527b244b24d548014ec86c204e4/packages/vike/src/server/runtime/renderPageServer/csp.ts#L45
-                  html = html.replace(/<script(\s|>)/g, `<script nonce="${nonce}"$1`)
-                }
-                return html
-              })(),
-            )
-          }
-        }
+export const WrappedApolloProvider = WrapApolloProvider(
+  buildManualDataTransport({
+    useInsertHtml() {
+      const stream = useStream()
+      const pageContext = usePageContext()
+      if (globalThis.__VIKE__IS_CLIENT) {
         return () => {}
-      },
-    }),
-  )
-}
+      }
+      assert(!pageContext.isClientSide)
+      assert(stream)
+      return (callback: () => React.ReactNode) => {
+        const nonce = (pageContext as any).cspNonce
+        stream.injectToStream(
+          // https://github.com/apollographql/apollo-client-nextjs/issues/325
+          (async () => {
+            let html = renderToString(await callback())
+            if (nonce) {
+              // No need to escape the injected HTML — see https://github.com/vikejs/vike/blob/36201ddad5f5b527b244b24d548014ec86c204e4/packages/vike/src/server/runtime/renderPageServer/csp.ts#L45
+              html = html.replace(/<script(\s|>)/g, `<script nonce="${nonce}"$1`)
+            }
+            return html
+          })(),
+        )
+      }
+    },
+  }),
+)
--- packages/vike-react-apollo/src/integration/Wrapper.tsx
+++ packages/vike-react-apollo/src/integration/Wrapper.tsx
@@ -3,13 +3,11 @@ export { Wrapper }
 import React, { type ReactNode } from 'react'
 import { usePageContext } from 'vike-react/usePageContext'
 import { assertUsage } from '../utils/assert.js'
-import { createWrappedApolloProvider } from './Transport.js'
-import type { PageContextServer } from 'vike/types'
+import { WrappedApolloProvider } from './Transport.js'
 
 function Wrapper({ children }: { children: ReactNode }) {
   const pageContext = usePageContext()
   const { ApolloClient: getApolloClient } = pageContext.config
   assertUsage(getApolloClient, 'Setting +ApolloClient is required')
-  const WrappedApolloProvider = createWrappedApolloProvider(pageContext as PageContextServer)
   return <WrappedApolloProvider makeClient={() => getApolloClient(pageContext)}>{children}</WrappedApolloProvider>
 }

c3ab90a00d11322feab3851a3f88601b1e260b41
Romuald Brillout <git@brillout.com>
2025-12-21 10:27:04 +0100

    Revert "bisec"
    This reverts commit fcdb29db7845beab2e9be72092b817e587f6b63f.

--- packages/vike-react/src/config.ts
+++ packages/vike-react/src/config.ts
@@ -35,8 +35,8 @@ const config = {
   hydrationCanBeAborted: true,
 
   // Remove <ClientOnly> children on server
+  ///*
   staticReplace: [
-    /*
     {
       env: 'server',
       type: 'call',
@@ -50,7 +50,6 @@ const config = {
       },
       remove: { arg: 1, prop: 'children' },
     },
-    */
     {
       env: 'server',
       type: 'call',
@@ -69,6 +68,7 @@ const config = {
       replace: { with: false },
     },
   ],
+  //*/
 
   // https://vike.dev/meta
   meta: {

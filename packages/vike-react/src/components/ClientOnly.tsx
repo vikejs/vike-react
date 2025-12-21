3728cc5a83ae1169ca0ac45ae1568ce090cd0467
Romuald Brillout <git@brillout.com>
2025-12-21 22:09:56 +0100

    expose useHydrated()

--- packages/vike-react/src/components/ClientOnly.tsx
+++ packages/vike-react/src/components/ClientOnly.tsx
@@ -1,8 +1,9 @@
 export { ClientOnly }
 
-import React, { useSyncExternalStore } from 'react'
+import React from 'react'
 import type { ReactNode } from 'react'
 import { usePageContext } from '../hooks/usePageContext.js'
+import { useHydrated } from '../hooks/useHydrated.js'
 import { assert } from '../utils/assert.js'
 
 /**
@@ -18,34 +19,4 @@ function ClientOnly({ children, fallback }: { children: ReactNode; fallback?: Re
   if (!pageContext.isClientSide) assert(children === undefined)
   const hydrated = useHydrated()
   return <>{hydrated ? children : fallback}</>
-}
-
-// Copied from https://github.com/sergiodxa/remix-utils/blob/33e6b04a08ef5f9b65e89c0a280c12b83762ef66/src/react/use-hydrated.ts#L17
-/**
- * Return a boolean indicating if the JS has been hydrated already.
- * When doing Server-Side Rendering, the result will always be false.
- * When doing Client-Side Rendering, the result will always be false on the
- * first render and true from then on. Even if a new component renders it will
- * always start with true.
- *
- * Example: Disable a button that needs JS to work.
- * ```tsx
- * let hydrated = useHydrated();
- * return (
- *   <button type="button" disabled={!hydrated} onClick={doSomethingCustom}>
- *     Click me
- *   </button>
- * );
- * ```
- */
-function useHydrated(): boolean {
-  return useSyncExternalStore(
-    subscribe,
-    () => true,
-    () => false,
-  )
-}
-
-function subscribe() {
-  return () => {}
 }

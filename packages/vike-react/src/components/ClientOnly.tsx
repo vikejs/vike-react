22209bd405dcc37dd12725c10fc452c2e19165d9
Romuald Brillout <git@brillout.com>
2025-12-21 21:25:28 +0100

    jsdocs

--- packages/vike-react/src/components/ClientOnly.tsx
+++ packages/vike-react/src/components/ClientOnly.tsx
@@ -20,6 +20,24 @@ function ClientOnly({ children, fallback }: { children: ReactNode; fallback?: Re
   return <>{hydrated ? children : fallback}</>
 }
 
+// Copied from https://github.com/sergiodxa/remix-utils/blob/33e6b04a08ef5f9b65e89c0a280c12b83762ef66/src/react/use-hydrated.ts#L17
+/**
+ * Return a boolean indicating if the JS has been hydrated already.
+ * When doing Server-Side Rendering, the result will always be false.
+ * When doing Client-Side Rendering, the result will always be false on the
+ * first render and true from then on. Even if a new component renders it will
+ * always start with true.
+ *
+ * Example: Disable a button that needs JS to work.
+ * ```tsx
+ * let hydrated = useHydrated();
+ * return (
+ *   <button type="button" disabled={!hydrated} onClick={doSomethingCustom}>
+ *     Click me
+ *   </button>
+ * );
+ * ```
+ */
 function useHydrated(): boolean {
   return useSyncExternalStore(
     subscribe,

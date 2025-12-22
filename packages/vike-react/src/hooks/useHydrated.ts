d52b6f45f66a3b24967536d402a9791ac46c4650
Romuald Brillout <git@brillout.com>
2025-12-22 15:45:20 +0100

    jsdoc

--- packages/vike-react/src/hooks/useHydrated.ts
+++ packages/vike-react/src/hooks/useHydrated.ts
@@ -4,15 +4,13 @@ import { useSyncExternalStore } from 'react'
 
 // Copied from https://github.com/sergiodxa/remix-utils/blob/33e6b04a08ef5f9b65e89c0a280c12b83762ef66/src/react/use-hydrated.ts
 /**
- * Return a boolean indicating if the JS has been hydrated already.
- * When doing Server-Side Rendering, the result will always be false.
- * When doing Client-Side Rendering, the result will always be false on the
- * first render and true from then on. Even if a new component renders it will
- * always start with true.
+ * Whether the page has already been hydrated.
+ *
+ * On the server, it always returns `false`. On the client, it returns `false` on first render and `true` after hydration completes.
  *
  * https://vike.dev/useHydrated
  *
- * Example: Disable a button that needs JS to work.
+ * Example: Disable a button that needs JavaScript to work.
  * ```tsx
  * const hydrated = useHydrated()
  * return (

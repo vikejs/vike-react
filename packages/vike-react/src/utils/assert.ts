224fc119225b2bc77958ca52be30b36df19fa989
Romuald Brillout <git@brillout.com>
2026-01-25 20:56:22 +0100

    fix: add clientOnly() deprecation warning

--- packages/vike-react/src/utils/assert.ts
+++ packages/vike-react/src/utils/assert.ts
@@ -2,3 +2,8 @@ export function assert(condition: unknown): asserts condition {
   if (condition) return
   throw new Error('You stumbled upon a vike-react bug, reach out on GitHub.')
 }
+
+export function assertWarning(condition: unknown, message: string) {
+  if (condition) return
+  console.warn(new Error(message))
+}

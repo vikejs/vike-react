b272b2b100d851606d7b39cb7fe571f28c863dfa
Romuald Brillout <git@brillout.com>
2026-01-25 20:58:04 +0100

    minor refactor

--- packages/vike-react/src/utils/assert.ts
+++ packages/vike-react/src/utils/assert.ts
@@ -1,9 +1,12 @@
-export function assert(condition: unknown): asserts condition {
+export { assert }
+export { assertWarning }
+
+function assert(condition: unknown): asserts condition {
   if (condition) return
   throw new Error('You stumbled upon a vike-react bug, reach out on GitHub.')
 }
 
-export function assertWarning(condition: unknown, message: string) {
+function assertWarning(condition: unknown, message: string) {
   if (condition) return
   console.warn(new Error(message))
 }

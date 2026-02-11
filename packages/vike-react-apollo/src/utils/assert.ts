3a2d25835178a8ba5ec7aaecee182bee33a944bf
Romuald Brillout <git@brillout.com>
2026-02-11 16:36:49 +0100

    minor

--- packages/vike-react-apollo/src/utils/assert.ts
+++ packages/vike-react-apollo/src/utils/assert.ts
@@ -3,10 +3,10 @@ export { assert }
 
 function assertUsage(condition: unknown, message: string): asserts condition {
   if (condition) return
-  throw new Error('Wrong usage: ' + message)
+  throw new Error('[vike-react-apollo] ' + message)
 }
 
 function assert(condition: unknown): asserts condition {
   if (condition) return
-  throw new Error('You stumbled upon a vike-react-apollo bug, reach out on GitHub.')
+  throw new Error('[vike-react-apollo] You stumbled upon a bug, reach out on GitHub.')
 }
--- packages/vike-react-query/src/utils/assert.ts
+++ packages/vike-react-query/src/utils/assert.ts
@@ -1,4 +1,4 @@
 export function assert(condition: unknown): asserts condition {
   if (condition) return
-  throw new Error('You stumbled upon a vike-react-query bug, reach out on GitHub.')
+  throw new Error('[vike-react-query] You stumbled upon a bug, reach out on GitHub.')
 }

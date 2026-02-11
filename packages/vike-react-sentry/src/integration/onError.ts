d1f12db6c481baeb9976c3bfa425c35c18cdbb27
Romuald Brillout <git@brillout.com>
2026-02-11 16:06:21 +0100

    minor refactor

--- packages/vike-react-sentry/src/integration/onError.ts
+++ packages/vike-react-sentry/src/integration/onError.ts
@@ -3,8 +3,7 @@ import type { Config } from 'vike/types'
 import { assert } from '../utils/assert.js'
 
 export const onError: Config['onError'] = (error) => {
-  if (Sentry.getClient()) {
-    assert(error && typeof error === 'object')
-    Sentry.captureException(error)
-  }
+  if (!Sentry.getClient()) return
+  assert(error && typeof error === 'object')
+  Sentry.captureException(error)
 }

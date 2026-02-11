bd0d1428b72f8cb477eb6dc4ee8303ead63dfa7a
Romuald Brillout <git@brillout.com>
2026-02-11 16:05:04 +0100

    don't use original error

--- packages/vike-react-sentry/src/integration/onError.ts
+++ packages/vike-react-sentry/src/integration/onError.ts
@@ -1,16 +1,10 @@
 import * as Sentry from '@sentry/node'
-import { isErrorCaptured } from '../utils/error.js'
 import type { Config } from 'vike/types'
 import { assert } from '../utils/assert.js'
 
 export const onError: Config['onError'] = (error) => {
-  assert(
-    error && typeof error === 'object' && 'getOriginalError' in error && typeof error.getOriginalError === 'function',
-  )
-  const original = error.getOriginalError()
-  if (Sentry.getClient() && !isErrorCaptured(original)) {
-    {
-      Sentry.captureException(original)
-    }
+  if (Sentry.getClient()) {
+    assert(error && typeof error === 'object')
+    Sentry.captureException(error)
   }
 }

3e6e1098f8ecd763a55ad57026e99c4833b4dd35
rtritto <ariele.1994@gmail.com>
2026-02-16 13:14:22 +0100

    feat: add useDataState

--- packages/vike-react/src/hooks/useData.tsx
+++ packages/vike-react/src/hooks/useData.tsx
@@ -2,6 +2,13 @@ export { useData }
 
 import { usePageContext } from './usePageContext.js'
 
+/**
+ * Access `pageContext.data` from any React component, and create a store with `pageContext.data` as initial data.
+ *
+ * See
+ * - https://vike.dev/data
+ * - https://vike.dev/pageContext-anywhere
+ */
 function useData<Data>(): Data {
   const data = usePageContext()?.data
   return data as any
---
+++ packages/vike-react/src/hooks/useDataState.tsx
@@ -0,0 +1,17 @@
+export { useDataState }
+
+import { type Dispatch, type SetStateAction, useState } from 'react'
+import { usePageContext } from './usePageContext.js'
+
+/**
+ * Access `pageContext.data` from any React component, and create a store with `pageContext.data` as initial data.
+ *
+ * See
+ * - https://vike.dev/data
+ * - https://vike.dev/pageContext-anywhere
+ */
+function useDataState<Data>(): [Data, Dispatch<SetStateAction<Data>>] {
+  const pageContext = usePageContext() as any;
+  const [data, setData] = useState(pageContext?.data);
+  return [data, setData];
+}
